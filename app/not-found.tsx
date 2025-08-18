a21cbf051cc3853f87483021478f4a2d341491e8
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-08-18 05:22:53 +0100

    creating and complate the page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+  return (
+    <>  
+          <h1>Not Found </h1>
+          <p>Sorry, we couldn't find the page you were looking for.</p>
+          <p>Please check the URL or return to the homepage.</p>
+            <a href="/products">Home page</a>
+    </>
+  );
+}
